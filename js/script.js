22eb197688e9ebfb6d925e8547ea5112168cf250
Danila <ppgpilot@yandex.ru>
2022-01-04 01:10:38 +0200

    самодельный слайдер

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@ const sliderWrap = document.querySelector('.slider__wrap');
 
 document.querySelector('.slider__next').addEventListener('click', function(){
     offset = offset + 653;
-    if (offset > 1306) {
+    if (offset > 1959) {
         offset = 0;
+    }
+    sliderWrap.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider__prev').addEventListener('click', function(){
+    offset = offset - 653;
+    if (offset < 0) {
+        offset = 1959;
     }
     sliderWrap.style.left = -offset + 'px';
 });
