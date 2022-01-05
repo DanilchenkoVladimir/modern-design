513b3ecc42450399ab1f172af81647881c817d6f
Vova <ppgpilot@yandex.ru>
2022-01-06 00:40:29 +0200

    добавлен слайдер в секцию reviews

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-let offset = 0; // смещение от левого края
-const sliderWrap = document.querySelector('.slider__wrap');
 
-document.querySelector('.slider__next').addEventListener('click', function(){
-    offset = offset + 653;
-    if (offset > 1959) {
-        offset = 0;
-    }
-    sliderWrap.style.left = -offset + 'px';
-});
+$('.wrap__slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    arrows: true
+});    
 
-document.querySelector('.slider__prev').addEventListener('click', function(){
-    offset = offset - 653;
-    if (offset < 0) {
-        offset = 1959;
-    }
-    sliderWrap.style.left = -offset + 'px';
-});
+$('.reviews__slider').slick({
+    dots: true,
+    arrows: true
+});        
+
+
+// let offset = 0; // смещение от левого края
+// const sliderWrap = document.querySelector('.slider__wrap');
+
+// document.querySelector('.slider__next').addEventListener('click', function(){
+//     offset = offset + 653;
+//     if (offset > 1959) {
+//         offset = 0;
+//     }
+//     sliderWrap.style.left = -offset + 'px';
+// });
+
+// document.querySelector('.slider__prev').addEventListener('click', function(){
+//     offset = offset - 653;
+//     if (offset < 0) {
+//         offset = 1959;
+//     }
+//     sliderWrap.style.left = -offset + 'px';
+// });
+
