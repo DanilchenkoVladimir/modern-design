e2779e9f2362c2bc597f279894d7c865050ed6bc
Vova <ppgpilot@yandex.ru>
2022-01-07 02:01:17 +0200

    обновлены отзывы

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 $('.main__slider').slick({
     slidesToShow: 1,
     dots: true,
@@ -9,24 +8,3 @@ $('.reviews__slider').slick({
     dots: true,
     arrows: true
 });        
-
-
-// let offset = 0; // смещение от левого края
-// const sliderWrap = document.querySelector('.slider__wrap');
-
-// document.querySelector('.slider__next').addEventListener('click', function(){
-//     offset = offset + 653;
-//     if (offset > 1959) {
-//         offset = 0;
-//     }
-//     sliderWrap.style.left = -offset + 'px';
-// });
-
-// document.querySelector('.slider__prev').addEventListener('click', function(){
-//     offset = offset - 653;
-//     if (offset < 0) {
-//         offset = 1959;
-//     }
-//     sliderWrap.style.left = -offset + 'px';
-// });
-
